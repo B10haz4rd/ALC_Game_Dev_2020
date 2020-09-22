7bb73e2a6815568fd2d20cee5260032059c3ed90
Logan Kennard <loganfkennard@gmail.com>
2020-09-22 08:48:13 -0600

    organized code and finished project up.

--- Prototype1-Driving Simulator/Assets/Scripts/FollowPlayer.cs
+++ Prototype1-Driving Simulator/Assets/Scripts/FollowPlayer.cs
@@ -7,15 +7,11 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5,-10);
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+  
     // Update is called once per frame
     void Update()
     {
-//Makes the Camera follow the player's position with an offset
+       //Makes the Camera follow the player's position with an offset
        transform.position = player.transform.position + offset;
     }
 }
--- Prototype1-Driving Simulator/Assets/Scripts/PlayerController.cs
+++ Prototype1-Driving Simulator/Assets/Scripts/PlayerController.cs
@@ -10,21 +10,17 @@ public class PlayerController: MonoBehaviour
     
     private float horizontalInput;
     private float forwardInput;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-    //Moves the car forward and back based on vertical input
+        
+        //Moves the car forward and back based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-    //Rotates the car left and right based on horizontal input
+        
+        //Rotates the car left and right based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
