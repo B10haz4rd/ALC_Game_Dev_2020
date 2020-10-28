392cb66e1303c3505f290ccc87488508484066c5
Logan Kennard <loganfkennard@gmail.com>
2020-10-28 09:12:35 -0600

    trying to use Iinumerator to fix the stupid spawn time...

--- Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -12,30 +12,43 @@ public class SpawnManagerX : MonoBehaviour
 
     private float startDelay = 1.0f;
 
-    private float spawnInterval = 4.0f;
+    private float spawnInterval;
+
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+
+        StartCoroutine(SpawnInterval());
     }
 
-    // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall()
+    void NewThing()
     {
-        //Generate ball at Random time?(needs more work...)
-        spawnInterval = Random.Range(2, 4);
-
-            // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
-        //generate random colored ball
-        int ballIndex= Random.Range(0, ballPrefabs.Length);
-
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
 
     }
 
+    IEnumerator SpawnInterval()
+    {
+        yeild return new WaitForSeconds(Random.Range(2, 4));
+
+        SpawnRandomBall();
+    }
+     // Spawn random ball at random x position at top of play area
+     void SpawnRandomBall()
+     {
+     // Generate random ball index and random spawn position
+     Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+     //generate random colored ball
+     int ballIndex = Random.Range(0, ballPrefabs.Length);
+
+     // instantiate ball at random spawn location
+     Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+     //Generate a ball at a random time
+     int spawnInterval = Random.Range(2, 4);
+
+     }
 
 }
