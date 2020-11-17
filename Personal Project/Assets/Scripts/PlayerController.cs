b5a6c3e912cc56d99508a4640fcc002692fa5e2a
Logan <loganfkennard@gmail.com>
2020-11-16 21:12:55 -0700

    worked on giving my player movement and then constraining it so it wouldn't leave the bounds of my personal project. it's working out better now.

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //speed of the player
-    private float speed = 10.0f;
+    private float speed = 30.0f;
+
+    //upper and lower bound limit
+    private float zBound = 24;
+    
+    //right/left bound limit
+    private float xRange = 24;
+
     //reference to the player's rigid body
     private Rigidbody playerRb;
     
@@ -19,6 +26,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    //Moves the player based on arrow key input
+    void MovePlayer()
+    {
         //controlls player movement based on keys pressed in correlation with the axis they are on the speed they can go and the directions they are allowed to travel.
         float horizontalInput = Input.GetAxis("Horizontal");
 
@@ -27,5 +41,29 @@ public class PlayerController : MonoBehaviour
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
 
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    //prevent the player from leaving the top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
+        //Placing bounds on the upward and downward directions.
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+         if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        //places boundry to the left
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //places a boundry to the right
+        if (transform.position.x > xRange)
+        {
+         transform.position = new Vector3(xRange, transform.position.y,transform.position.z);
+        }
     }
 }
