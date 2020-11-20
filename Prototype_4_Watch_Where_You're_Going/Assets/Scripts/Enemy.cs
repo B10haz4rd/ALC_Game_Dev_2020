a7f9df8782af397a96e557b819920411b3099aec
Logan Kennard <loganfkennard@gmail.com>
2020-11-20 09:09:56 -0700

    worked on making a difficult enemy AI and I
    began working on making it spawn randomly

---
+++ Prototype_4_Watch_Where_You're_Going/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private Rigidbody enemyRB;
+
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //retrieve the ENEMYRIGIDBODY and the PLAYER
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move toward the player by subracting the distance between the PLAYER and the ENEMY multiplied by the SPEED variable
+
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRB.AddForce(lookDirection * speed);
+    }
+}
