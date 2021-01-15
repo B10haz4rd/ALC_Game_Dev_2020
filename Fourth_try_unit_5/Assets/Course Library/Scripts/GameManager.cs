03e4bd6b3279d0d081214879c09e801196f703d9
Logan Kennard <loganfkennard@gmail.com>
2021-01-15 15:25:51 -0700

    worked on the files some more almost there...

--- Fourth_try_unit_5/Assets/Course Library/Scripts/GameManager.cs
+++ Fourth_try_unit_5/Assets/Course Library/Scripts/GameManager.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
 
+    public TextMeshProUGUI scoreText;
+
+    public TextMeshProUGUI gameOverText;
+
+    public Button restartButton;
+
+    public bool isGameActive;
+
+    private int score;
+
     private float spawnRate = 1.0f;
     
     // Start is called before the first frame update
     void Start()
-    {
-        StartCoroutine(SpawnTarget());
+    { 
+        
     }
 
     // Update is called once per frame
@@ -31,13 +44,42 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while(true)
+        while(isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0,targets.Count);
             Instantiate(targets[index]);
         }
         
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame()
+    {
+        isGameActive = true;
+
+        StartCoroutine(SpawnTarget());
+
+        UpdateScore(0);
+
+        score = 0;
     }
     
 }
